b2a8d8b8bb61cba8425eb351d588934a0bed4279
Jenny HY Lin <jennyhylin@gmail.com>
2023-07-21 14:39:36 +1200

    Removed test images & single cpd app done

--- app_image_v.1.py
+++ app_image_v.1.py
@@ -6,16 +6,21 @@ import pandas as pd
 import polars as pl
 from rdkit import Chem
 from rdkit.Chem import Draw
+#from rdkit.Chem.Draw import IPythonConsole
+#from rdkit.Chem.rdmolfiles import SmilesWriter, SmilesMolSupplier
 from rdkit.Chem.Draw import rdMolDraw2D
 
 import datamol as dm
 
 from pathlib import Path
+#import io
 from PIL import Image
+# IPython console similar to Jupyter notebook
+# from IPython.display import Image
 
+#from shiny import App, render, ui
 from shiny import App, Inputs, Outputs, Session, render, ui, reactive
 from shiny.types import ImgData
-
 
 # Data source---
 # Pandas library renders df columns in strange data types
@@ -28,7 +33,6 @@ mols = list(mols)
 
 
 # Input--- **work-in-progress**
-# TODO: input types ready, need to set up page style e.g. add columns etc.
 app_ui = ui.page_fluid(
     ui.input_numeric("mol", "Molecule index:", 0, min=0, max=143),
     ui.input_text("filename", "File name of PNG file:"),
@@ -37,31 +41,62 @@ app_ui = ui.page_fluid(
 )
 
 # Output--- **work-in-progress**
-# Below code works for inputting one molecule, saved as PNG file with image shown in app
 def server(input, output, session):
     @output
+    # Not requiring to render text as only images are needed as final output
+    # If using both render.text & render.image at the same time,
+    # only PNG file of specified molecule will be saved with no image shown!
+    #@render.text
     @render.image
     def image():
+        # MolsToGridImage() works in Jupyter notebook only (returns IPython.core.display.Image object)
+        #img = Draw.MolsToGridImage(mols) 
+
+        # --Testing MolDraw2DCairo
+        # Saving 2D compound image as PNG
+        # drawer = rdMolDraw2D.MolDraw2DCairo(2000,2000,300,300)
+        # Change to black & white
+        # drawer.drawOptions().useBWAtomPalette()
+        # drawer.DrawMolecules(mols)
+        # drawer.FinishDrawing()
+        # drawer.WriteDrawingText('anti-inf.png')
+
+        # Open the PNG file to show image (for MacOS - opens file using Preview)
+        # image_test = Image.open("anti-inf.png")
+        # image_test.show()
+        # For image of list of molecules: ?improve image resolution (blurry at the moment)
+
+        # --Testing MolsToImage
+        #Image of molecules all aligned horizontally (not in grid/table format)
+        # img_test = Draw.MolsToImage(mols, fitImage = True)
+        # img_test
+        # img_test.save("antiinf.png")
+        # image_new = Image.open("antiinf.png")
+        # image_new.show()
+
         # Place action button here to take a reactive dependency
         input.btn()
 
-        # Using MolToFile() - only saving a single compound as PNG file (via index position)
+        # MolToFile() - only saving a single compound as PNG file (via index position)
+        # Draft function v.1 - saving specified compound as PNG file:
+        # index = index position number of each compound
+        # file_name = name of PNG file to be saved
 
-        # Use reactive.isolate to not take a reactive dependency,
+        # **this part works with PNG file saved as input number**
+        # Use reactive.isolate to not take a reactive dependency 
         # when entering/specifying compound and name of PNG file saved
-        # only execute the PNG file generating & saving for the specified compound
-        # when pressing the "Confirm" action button
         with reactive.isolate():
             Draw.MolToFile(mols[input.mol()], f"{input.filename()}.png")
 
-
-        # TODO: ?can somehow formulate to take in 2 PNG images (or more e.g. 4)
-
-        # --Testing MolToFile - 4 PNG files
+        
+        # --Testing MolToFile 
         # Draw.MolToFile(mols[0], "af1.png")
         # Draw.MolToFile(mols[1], "af2.png")
         # Draw.MolToFile(mols[2], "af3.png")
         # Draw.MolToFile(mols[3], "af4.png")
+
+
+        #img = Image.open(f"{input.filename()}.png")
 
         # --Using PIL/Pillow to manipulate images
         # img1 = Image.open("af1.png")
@@ -82,11 +117,21 @@ def server(input, output, session):
         #blank_image.save("merged.png")
 
 
-        # Show image of saved PNG file of specified compound from input
+        # --Use local file path to import PNG image file
+        # Showing a list of molecules
+        # dir = Path(__file__).resolve().parent
+        # img: ImgData = {"src": str(dir / "anti-inf.png"), "width": "2350px", "height": "2350px"}
+        # return img
+
+        # TODO: **Not showing up in app yet!** ?to add reactive effect
+        # Showing image from the saved PNG file
+        
         dir = Path(__file__).resolve().parent
         img: ImgData = {"src": str(dir / f"{input.filename()}.png")}
         return img
                 
+
+        #return image
 
 
 app = App(app_ui, server)
